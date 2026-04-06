e10fe45f4bbd970113fc3122de4a91940369079f
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-06 22:13:10 +0530

    Add JavaScript data type examples and console logs
    Added examples of JavaScript data types and console logs.

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,1 +1,24 @@
+ a=67;
+console.log(a);
+console.log("hii");
+//alert (3+4) // we are using node.js not browser
+console.log(3+
+    6) // code readability that should be high
+console.log("Priya")
+let name="hi"
+let age=21
+let isloggin=false
+//Primitive Datatype
 
+// number=>2 to power 53
+//bigint
+//string=>""
+//boolean=true/false
+//null=> standalone value
+//undefined=> abhi value assign nhi ki h
+//symbol=>unique
+
+//object
+console.log(typeof(3))
+console.log(typeof(null))
+console.log(typeof(undefined))
