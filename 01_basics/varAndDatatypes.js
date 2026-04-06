2494e7054c7c1b26aad48b3e64b9b49e2533eace
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-06 21:38:44 +0530

    Add account variables and log their values
    Added variable declarations and updated values for account details.

--- 01_basics/varAndDatatypes.js
+++ 01_basics/varAndDatatypes.js
@@ -1,1 +1,18 @@
 console.log("Hello, World!");
+console.log("Hello, World!");
+const accountId = 100;
+let accountEmail = "priyanshi@gmail.com";
+var accountPassword = "12345";
+let accountCity = "Etawah";
+//accountId=67; //not allowed
+accountEmail = "geekforgeek.com";
+accountPassword = "789";
+accountCity = "Delhi";
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId, accountPassword, accountEmail, accountCity]);
+// Prefer not to use var because of issue in block scope and functional scope
+ let accountState;
+ console.log(accountState)
