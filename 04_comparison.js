68cc99ea5eee3d33575fd4bf1bf4a10f44529c60
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-06 23:36:19 +0530

    Update 04_comparison.js

--- 04_comparison.js
+++ 04_comparison.js
@@ -1,1 +1,14 @@
+console.log("2"<3)
+console.log(null>0)
+console.log(null<0)
+console.log(null==0)
+console.log(null>=0)
+console.log(null<=0)
+console.log(undefined<0)
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined<=0)
 
+//===
+console.log("2"===2)
+console.log("2"==2)
