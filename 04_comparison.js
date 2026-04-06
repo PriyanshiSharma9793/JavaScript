a4ac0d86402a2ff79eb070a47be630b9addad66e
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-06 23:52:02 +0530

    Update 04_comparison.js

--- 04_comparison.js
+++ 04_comparison.js
@@ -12,3 +12,25 @@ console.log(undefined<=0)
 //===
 console.log("2"===2)
 console.log("2"==2)
+
+//Primitive is call by value datatype
+//7 types: String,Number,Undefined,Null,Boolean,Symbol,BigInt
+
+//Non Primitive  is call by reference datatype
+//Array,Objects,Functions
+
+const id =Symbol('123')
+const id2=Symbol('123')
+console.log(id===id2)
+
+//Array
+const heroes=["Priya","nandini","kratika"]
+//Objects
+let Myobj={
+    name:"Priya",
+    class:"seventh",
+}
+//Functions
+const myfunction= function(){
+    console.log("HII")
+}
