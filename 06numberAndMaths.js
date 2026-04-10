42f1cf1db73db3fba53f87ccebc4d5066a7632c0
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-10 06:08:37 +0530

    Update 06numberAndMaths.js

--- 06numberAndMaths.js
+++ 06numberAndMaths.js
@@ -1,1 +1,32 @@
+// const score=300;
+// console.log(score)
 
+// const balance =new Number(100) //explicitly defined
+// console.log(balance)
+// console.log(score.toString().length) //no. converted into String
+// console.log(balance.length)  // Becoz no. is not a string
+// console.log(balance.toFixed(2))
+// console.log(score.toFixed(2))
+
+// const otherno=123.7889
+// console.log(otherno.toPrecision(3))
+
+// const otherno1=1123.7889
+// console.log(otherno1.toPrecision(3))
+
+// const hundred=1000000000
+// console.log(hundred.toLocaleString('en-IN')) //indian 
+
+ //************Maths*************
+
+ console.log(Math)
+ console.log(Math.abs(-7))
+console.log(Math.round(6.89))
+console.log(Math.ceil(2.4))
+console.log(Math.floor(6.9))
+console.log(Math.min(5,8,9))
+console.log(Math.random());
+
+const min =10;
+const max=20;
+console.log(Math.floor(Math.random()*(max -min+1))+min)
