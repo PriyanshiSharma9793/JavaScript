74d1f6adbb71484a7fe325dfc0f574db46559dee
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-12 15:05:53 +0530

    Update 11_this&arrow( ).js

--- 11_this&arrow( ).js
+++ 11_this&arrow( ).js
@@ -1,1 +1,42 @@
+const a={
+    username:"Priyanshi",
+    Price:999,
+    welcomeMess:function(){
+        console.log(`${this.username} ,Welcome to website`)
+        console.log(this)
+    }
+  
+}
+//a.welcomeMess()
+//a.username="sam"
+//a.welcomeMess()
+//console.log(this) 
 
+// function chai(){
+//     let u="Priya"
+//     console.log(this.u)
+// }
+// chai()
+// Same code but doifferent way to define function
+// const chai= function(){
+//    let u ="Priya"
+//     console.log(this.u)
+//  }
+// chai()
+
+ const chai= ()=>{
+    let u ="Priya"
+    console.log(this)
+  }
+chai()
+
+//++++++++++Arrow Function+++++++++++
+//Basic 
+const add2=(num1,num2) =>{
+    return num1+num2;
+}
+console.log(add2(3,4))
+
+//implicit return
+const addtwo=(num1,num2)=>(num1+num2)
+console.log(addtwo(5,7))
