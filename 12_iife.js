c6a909d7eae6f4c7796bea1062c7c4bcf55c9a4d
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-12 16:51:06 +0530

    Update 12_iife.js

--- 12_iife.js
+++ 12_iife.js
@@ -1,9 +1,11 @@
 //Immediately Invoked function Expressions(IIFE)
 //Global scope ke pollution ko hatane ke liye iife ka use krte h
 (function chai(){
+    //named iife
     console.log(`DB CONNECTED`)
 })();//FOR ENDING THATS WHY WE USES;
 
 ( (name) =>{
+    //simple iife
     console.log(`DB CONNECTED ${name}`);
 } )('Priya')
