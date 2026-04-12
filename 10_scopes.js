4451be11b2ef3db04c3cd63a8497c3b63690c08f
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-12 11:34:19 +0530

    Update 10_scopes.js

--- 10_scopes.js
+++ 10_scopes.js
@@ -10,3 +10,39 @@ console.log("Inner:",a);
 console.log(a)
 //console.log(b)
 //console.log(c)
+
+//*******Nested Scope**********
+function one(){
+    const user="priya"
+    function two(){
+       const website="utube"
+       console.log(user)
+    }
+   //console.log(website)
+   two()
+}
+one()
+if(true){
+    const username = "Priyanshi"
+    if(username === "Priyanshi"){
+        const website =" Utube"
+        console.log(username + website)
+    }
+
+    //console.log(website) scope ke bahar access nhi hoga
+}
+//console.log(username) scope ke bahar access  nhi hoga
+
+//++++++++++++ interesting++++++++++++++
+console.log(addone(6))//it will access before function declaration
+function addone(num){
+      return num +1
+}
+//addone(6)
+//expression ,store in variable
+// it will not accesible before function declaration we generally known as hoisting
+//console.log(addtwo(7))
+const addtwo =function(num){
+    return num+2;
+}
+//addtwo(7)
