39a4e5a22dccf271e3a157d5c9ab788e89d195d0
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-14 21:17:32 +0530

    Update 14_switch.js

--- 14_switch.js
+++ 14_switch.js
@@ -1,1 +1,44 @@
+// //switch(key){
+// case value:
+//     break;
+// default:
+//     break;
+// }
+const month = "mar"
+switch(month){
+    case "jan":
+    console.log("jan")
+    break;
+    case "feb":
+    console.log("feb")
+    break;
+    case "mar":
+    console.log("march")
+    break;
+    case "apr":
+    console.log("april")
+    break;
+    default:
+    console.log("Nothing")
+    
+}
 
+//++++++++++++Notes++++++++++++
+//All are false- false , 0, -0, BigInt  0n, null, undefined ,NaN, ""(without space)
+//Otherwise is true- [], "0", 'false', " "(space), {}, function(){}
+
+const useremail=[]
+if(useremail){
+    console.log("Got user email");
+}else{
+    console.log("Dont have user email");
+}
+if(useremail.length===0){
+    console.log("array is empty")
+}
+
+//++++++++++Empty Object++++++++++++
+const emptyObj={}
+ if(Object.keys(emptyObj).length===0){
+       console.log("object is empty");
+ }
