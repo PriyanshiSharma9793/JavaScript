600289051962d78f0a14d7f2420fa9eea7b558a4
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-14 21:28:43 +0530

    Update 14_switch.js

--- 14_switch.js
+++ 14_switch.js
@@ -42,3 +42,18 @@ const emptyObj={}
  if(Object.keys(emptyObj).length===0){
        console.log("object is empty");
  }
+
+ //********Nullish Coalescing Operator (??):null undefined*************
+
+ let val1;
+ //val1 = 5 ?? 10 o/p-5
+ //val1 = null ?? 10; o/p-10
+//val1 = undefined ?? 15; o/p-15
+ val1=null ?? 10 ??15;
+ console.log(val1)
+
+ // *******************Terniary Operator*********************
+//condition ? true:false
+const ice= 100;
+ice >=80 ? console.log("more than 80") : console.log("less than 80")
+ 
