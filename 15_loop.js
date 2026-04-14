1ac3311b82367de95c81ca6c31e3dc942a48c93a
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-14 22:29:50 +0530

    Update 15_loop.js

--- 15_loop.js
+++ 15_loop.js
@@ -1,1 +1,19 @@
+//********for loop with break and continue**************** 
+//for
 
+for(let i=0; i <= 10;i++){
+    const element = i;
+    if(element==5){
+        //console.log("5 is best no.")
+    }
+    //console.log(element)
+}
+//console.log(element) outside scope not working
+
+for(let i=1; i <= 10;i++){
+    //console.log(`outer loop ${i}`)
+    for(let j=1; j <= 10;j++){
+    // console.log(`inner loop ${j} and inner loop ${i}`)
+    //console.log(i + '*' + j + '=' + i*j)
+}
+}
