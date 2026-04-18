637835594d501584a32ca72f00bf03d4555f6d47
Priyanshi Sharma <sharmepriyanshi@gmail.com>
2026-04-18 15:08:05 +0530

    Create 13_while&dowhile.js

---
+++ 13_while&dowhile.js
@@ -0,0 +1,21 @@
+//***********While Loop***********
+let i = 0;            
+
+while(i <= 10) {       
+    console.log(`Value of index is ${i}`);  
+    i = i + 2;         
+}
+let myarr=['f','h','e','d']
+let arr=0;
+while(arr<myarr.length){
+    console.log(`value  is ${myarr[arr]}`);
+    arr++;
+
+}
+//******dowhile*********** */
+let score=11;
+do{
+     console.log(`Score is ${score}`);
+     score++;
+}while(score<10);
+
